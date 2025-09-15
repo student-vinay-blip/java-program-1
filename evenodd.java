e61265a458452baf696cddd7fcb1d82b715d47e4
student-vinay-blip <vinaypatil98765@gmail.com>
2025-09-15 22:34:41 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class evenodd{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("EVEN NUMBER");
+
+
+        }
+        else{
+            System.out.println("ODD NUMBER");
+        }
+
+
+
+
+    }
+}
+    
+
