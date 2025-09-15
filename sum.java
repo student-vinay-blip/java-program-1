88a95298f8769f94992bb8c1ecf3ffa1e0ef3e19
student-vinay-blip <vinaypatil98765@gmail.com>
2025-09-15 22:15:15 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum {
+    public static void main(String[]args){
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a first number");
+        a=sc.nextInt();
+        System.out.println("Enter a second number");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("the sum of two number is"+c);
+    }
+
+     
+}
