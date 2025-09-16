5587508de0a234a1692fa8fc9e7279e6e18a5c37
student-vinay-blip <vinaypatil98765@gmail.com>
2025-09-16 20:22:43 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+    public static void main(String[] args){
+        int fact=1,num=5;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+
+        }
+        System.out.println("factorial of "+num+" is "+fact);
+    }
+    
+}
