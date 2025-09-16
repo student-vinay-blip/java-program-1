e4fbb2b9acdf7e469cb38daaf3150719d0ec915a
student-vinay-blip <vinaypatil98765@gmail.com>
2025-09-16 20:16:37 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,19 @@
+public class largest {
+    public static void main(String[] args){
+        int a=25,b=16,c=50;
+        if(a>b && a>c){
+            System.out.println("a is largest");
+        }
+        else if(b>c && b>a){
+            System.out.println("b is largest");
+
+
+            
+        
+        }    else
+        {
+            System.out.println("c is Largest");
+        }
+        }
+    }
+    
