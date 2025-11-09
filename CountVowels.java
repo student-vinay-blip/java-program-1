b7cd316a216b0a19408578f98a53966556e347a6
vinaypatil7499 <vinaypatil98765@gmail.com>
2025-11-09 22:29:31 +0530

    Add files via upload

---
+++ CountVowels.java
@@ -0,0 +1,24 @@
+// CountVowels.java
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+
+        int vowels = 0, consonants = 0;
+        for (char c : str.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiou".indexOf(c) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        sc.close();
+    }
+}
